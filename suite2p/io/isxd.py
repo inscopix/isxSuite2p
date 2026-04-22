cab429ac58c03ad590a62f0585b618ddcb94ce73
Nosheen Adil <nosheen.0398@gmail.com>
2026-04-22 11:57:15 -0700

    chore: more updates isxd_to_binary

--- suite2p/io/isxd.py
+++ suite2p/io/isxd.py
@@ -30,15 +30,16 @@ def isxd_to_binary(dbs, settings, reg_file, reg_file_chan2):
     if not HAS_ISX:
         raise ImportError("Inscopix isx is required for this file type, please 'pip install isx'")
 
-    ops1 = utils.init_ops(ops)
+    dbs = utils.init_dbs(dbs)
     # the following should be taken from the metadata and not needed but the files are initialized before...
-    nplanes = ops1[0]["nplanes"]
-    nchannels = ops1[0]["nchannels"]
+    nplanes = dbs[0]["nplanes"]
+    nchannels = dbs[0]["nchannels"]
     # open all binary files for writing
-    ops1, file_list, reg_file, reg_file_chan2 = utils.find_files_open_binaries(ops1)
+    # ops1, file_list, reg_file, reg_file_chan2 = utils.find_files_open_binaries(ops1)
+    file_list = dbs[0]["filelist"]
     iall = 0
-    for j in range(ops1[0]["nplanes"]):
-        ops1[j]["nframes_per_folder"] = np.zeros(len(file_list), np.int32)
+    for j in range(dbs[0]["nplanes"]):
+        dbs[j]["nframes_per_folder"] = np.zeros(len(file_list), np.int32)
     ik = 0
 
     for ifile, fname in enumerate(file_list):
@@ -46,13 +47,13 @@ def isxd_to_binary(dbs, settings, reg_file, reg_file_chan2):
         nplanes = 1  #f.shape[1]
         nchannels = 1  #f.shape[2]
         nframes = f.timing.num_samples
-        iblocks = np.arange(0, nframes, ops1[0]["batch_size"])
+        iblocks = np.arange(0, nframes, dbs[0]["batch_size"])
         if iblocks[-1] < nframes:
             iblocks = np.append(iblocks, nframes)
 
         # data = nframes x nplanes x nchannels x pixels x pixels
         if nchannels > 1:
-            nfunc = ops1[0]["functional_chan"] - 1
+            nfunc = dbs[0]["functional_chan"] - 1
         else:
             nfunc = 0
         # loop over all frames
@@ -65,42 +66,42 @@ def isxd_to_binary(dbs, settings, reg_file, reg_file_chan2):
                 im2write = im[:]
                 for j in range(0, nplanes):
                     if iall == 0:
-                        ops1[j]["meanImg"] = np.zeros((im.shape[1], im.shape[2]),
+                        dbs[j]["meanImg"] = np.zeros((im.shape[1], im.shape[2]),
                                                       np.float32)
                         if nchannels > 1:
-                            ops1[j]["meanImg_chan2"] = np.zeros(
+                            dbs[j]["meanImg_chan2"] = np.zeros(
                                 (im.shape[1], im.shape[2]), np.float32)
-                        ops1[j]["nframes"] = 0
+                        dbs[j]["nframes"] = 0
                     if ichan == nfunc:
-                        ops1[j]["meanImg"] += np.squeeze(im2mean)
+                        dbs[j]["meanImg"] += np.squeeze(im2mean)
                         reg_file[j].write(
                             bytearray(im2write[:].astype("int16")))
                     else:
-                        ops1[j]["meanImg_chan2"] += np.squeeze(im2mean)
+                        dbs[j]["meanImg_chan2"] += np.squeeze(im2mean)
                         reg_file_chan2[j].write(
                             bytearray(im2write[:].astype("int16")))
 
-                    ops1[j]["nframes"] += im2write.shape[0]
-                    ops1[j]["nframes_per_folder"][ifile] += im2write.shape[0]
+                    dbs[j]["nframes"] += im2write.shape[0]
+                    dbs[j]["nframes_per_folder"][ifile] += im2write.shape[0]
             ik += nframes
             iall += nframes
 
     # write ops files
-    do_registration = ops1[0]["do_registration"]
-    do_nonrigid = ops1[0]["nonrigid"]
-    for ops in ops1:
-        ops["Ly"] = im.shape[1]
-        ops["Lx"] = im.shape[2]
+    do_registration = settings["run"]["do_registration"]
+    # do_nonrigid = ops1[0]["nonrigid"]
+    for db in dbs:
+        db["Ly"] = im.shape[1]
+        db["Lx"] = im.shape[2]
         if not do_registration:
-            ops["yrange"] = np.array([0, ops["Ly"]])
-            ops["xrange"] = np.array([0, ops["Lx"]])
+            db["yrange"] = np.array([0, db["Ly"]])
+            db["xrange"] = np.array([0, db["Lx"]])
         #ops["meanImg"] /= ops["nframes"]
         #if nchannels>1:
         #    ops["meanImg_chan2"] /= ops["nframes"]
-        np.save(ops["ops_path"], ops)
+        np.save(db["ops_path"], db)
     # close all binary files and write ops files
     for j in range(0, nplanes):
         reg_file[j].close()
         if nchannels > 1:
             reg_file_chan2[j].close()
-    return ops1[0]
+    return dbs[0]
